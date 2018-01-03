aea7097985ae63062720a92ab0b49a023a7bddbf
Bill <iamkg05@gmail.com>
2018-01-02 21:35:31 -0600

    leave default layout while display error

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occured</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
+  <div id="app" class="app">
+    <AppHeader></AppHeader>
+    <main id="main-content" class="main-content">
+    </main>
+    <AppFooter></AppFooter>
+    <div class="container">
+      <h1 v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occured</h1>
+      <nuxt-link to="/">Home page</nuxt-link>
+    </div>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
 export default {
-  props: ['error']
+  props: ['error'],
+  data () {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  components: {
+    AppHeader,
+    AppFooter
+  }
 }
 </script>
 
