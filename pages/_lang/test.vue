8180bc77af1f5944ef003b11002f56fe20ef3cbe
hugoinperson <hugoinperson@gmail.com>
2018-02-08 10:40:02 -0600

    add test page for testing out components in dev

---
+++ pages/_lang/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>This is the test page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {}
+}
+</script>
+
+<style scoped lang="scss">
+@import 'assets/css/app';
+@import 'assets/css/colors';
+@import 'assets/css/typography';
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,6 @@
+<script>
+// the real first page of the site is actuall 'index.vue' under '_lang' folder,
+// so that every page of the site is under the selected language context.
+import Test from '~/pages/_lang/test'
+export default Test
+</script>
