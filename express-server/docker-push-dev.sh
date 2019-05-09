371e84f5181dc38a010b1e18f9c1235fb4807e4e
Robin Liao <yilia@microsoft.com>
2019-05-09 03:06:03 -0700

    move docker cred to .env

--- express-server/docker-push-dev.sh
+++ express-server/docker-push-dev.sh
@@ -1,7 +1,11 @@
+source ./express-server/.env
+
 HOST="acttaiwan.azurecr.io"
 IMG_TAG="ustw-site-dev:latest"
-USER="acttaiwan"
-PASS=""
+USER=$ACR_USER
+PASS=$ACR_PASS
+
+echo "[Azure Container Registry] USER=$USER  PASS=$PASS"
 
 docker build -t $IMG_TAG . -f express-server/Dockerfile
 docker login $HOST -u $USER -p $PASS
